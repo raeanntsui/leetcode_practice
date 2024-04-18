604b2e76ed96991311cc58c6a41de4f99b99bdac
Raeann <raeann.tsui@gmail.com>
2024-04-18 11:33:16 -0700

    adding files to two sum practice folder

---
+++ two_sum/two_sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+nums = [2, 7, 11, 15]        
