f0f8accaddd8176d63174cbe18ab3e0b38da5a30
Raeann <raeann.tsui@gmail.com>
2025-03-05 16:03:57 -0800

    checkpoint for romanToInt

--- javascript_practice/romanToInt.js
+++ javascript_practice/romanToInt.js
@@ -1,6 +1,24 @@
 function romanToInt(str) {
-  const dict = { IV: 4, IX: 9, XL: 40, XC: 90, CD: 400, CM: 900 };
-  let char = "star";
+  const dict = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
+    "IV": 4,
+    "IX": 9,
+    "XL": 40,
+    "XC": 90,
+    "CD": 400,
+    "CM": 900,
+  };
+
+  let total = 0;
+  let i = 0;
+
+  for (let i = 0; i < str.length; i++) {}
 }
 
 romanToInt("III");
